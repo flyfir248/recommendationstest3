427edee8847ba4d8149f9b5b1b9310f239f392b9
anoop <anoopjohny2000@gmail.com>
2024-05-23 14:15:13 +0530

    push

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+# Load the CSV data
+data = pd.read_csv('amz_fpkt_data.csv')
+
+# Home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Search route
+@app.route('/search', methods=['GET'])
+def search():
+    query = request.args.get('query')
+    if query:
+        results = data[data['deal_title'].str.contains(query, case=False, na=False)]
+    else:
+        results = pd.DataFrame()
+    return render_template('search.html', query=query, results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)

05b7e44846404530303c012a9d25847247f93c7b
anoop <anoopjohny2000@gmail.com>
2024-05-27 13:26:58 +0530

    push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
+import random
 
 app = Flask(__name__)
 
@@ -9,7 +10,9 @@ data = pd.read_csv('amz_fpkt_data.csv')
 # Home route
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Randomly select a few products for the featured section
+    featured_products = data.sample(n=5).to_dict(orient='records')
+    return render_template('index.html', featured_products=featured_products)
 
 # Search route
 @app.route('/search', methods=['GET'])
@@ -21,6 +24,15 @@ def search():
         results = pd.DataFrame()
     return render_template('search.html', query=query, results=results)
 
+# Autocomplete route
+@app.route('/autocomplete', methods=['GET'])
+def autocomplete():
+    query = request.args.get('query', '')
+    if query:
+        suggestions = data[data['deal_title'].str.contains(query, case=False, na=False)]['deal_title'].unique().tolist()
+    else:
+        suggestions = []
+    return jsonify(suggestions)
+
 if __name__ == '__main__':
     app.run(debug=False)
-
